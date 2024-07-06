27e1dff59fc85369dc0d204184adc1da19fec38d
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-07-06 01:35:34 -0500

    statistics controller

---
+++ FASTTAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,49 @@
+﻿using FASTTAPI.DataLayer.DataTransferObjects;
+using FASTTAPI.DataLayer.PostgresSqlRepositories;
+using FASTTAPI.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace FASTTAPI.Controllers
+{
+    [ApiController]
+    [Route("AirlineAircrafts")]
+    public class AirlineAircraftsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly AirlineAircraftsPostgresSqlRepository _airlineAircraftPostgresSqlRepository;
+
+        public AirlineAircraftsController(IConfiguration config)
+        {
+            _configuration = config;
+            _airlineAircraftPostgresSqlRepository = new AirlineAircraftsPostgresSqlRepository();
+        }
+
+        [HttpGet]
+        public async Task<List<AirlineAircraft>> Get()
+        {
+            using (var connection = new NpgsqlConnection(DatabaseConnectionStringBuilder.GetSqlConnectionString(_configuration)))
+            {
+                connection.Open();
+                return _airlineAircraftPostgresSqlRepository.GetAirlineAircrafts(connection);
+                connection.Close();
+            }
+        }
+
+        [HttpPut]
+        public async Task Put([FromBody] AirlineAircraftPutBody body)
+        {
+            using (var connection = new NpgsqlConnection(DatabaseConnectionStringBuilder.GetSqlConnectionString(_configuration)))
+            {
+                connection.Open();
+                NpgsqlTransaction trans = connection.BeginTransaction();
+                foreach (var airlineAircraft in body.AirlineAircrafts)
+                {
+                    _airlineAircraftPostgresSqlRepository.UpdateAirlineAircraft(airlineAircraft, connection, trans);
+                }
+                trans.Commit();
+                connection.Close();
+            }
+        }
+    }
+}
